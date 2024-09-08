23801d79d1c7e787f3606a6d4d21cbca329add5b
Carl S <lerhinox@gmail.com>
2024-09-08 12:59:19 +0200

    Add GitHub Actions workflow and update Astro config

--- src/data/activityData.ts
+++ src/data/activityData.ts
@@ -8,7 +8,6 @@ export interface DayActivity {
 }
 
 export const ACTIVITY_DATA: DayActivity[] = [
-{ date: '2024-09-08', sports: true, twitter: true, work: true, growth: true, note: 'To be updated' },
   {
     date: '2024-09-04',
     sports: true,
