cd5dceab2fa979f00e10adf0e4311301db3384af
carluzh <carl.schmidt@uzh.ch>
2024-10-07 11:46:47 +0200

    Updated Projects

--- src/data/activities.ts
+++ src/data/activities.ts
@@ -65,7 +65,7 @@ export const ACTIVITIES = [
     todos: [
       {
         title: "Initial Research",
-        percentage: 25,
+        percentage: 75,
         items: [
           { text: "Idea and General Knowledge", done: 1 },
           { text: "Finding Articles, Content, etc.", done: 1 },
@@ -90,7 +90,7 @@ export const ACTIVITIES = [
     todos: [
       {
         title: "Influence",
-        percentage: 40,
+        percentage: 80,
         items: [
           { text: "Fund Argent Wallet", done: 1 },
           { text: "Build Miners and Warehouses", done: 1 },
@@ -101,7 +101,7 @@ export const ACTIVITIES = [
       },
       {
         title: "Loot Survivor",
-        percentage: 80,
+        percentage: 100,
         items: [
           { text: "Read through Documents", done: 1 },
           { text: "Start playing off-chain", done: 1 },
@@ -110,107 +110,63 @@ export const ACTIVITIES = [
         ]
       },
       {
-        title: "Document",
-        percentage: 0,
+        title: "Writing",
+        percentage: 60,
         items: [
-          { text: "Jot down learnings, benefits and downsides", done: 0 }
-        ]
-      }
-    ]
-  }
+          { text: "Create Structure/Outline", done: 1 },
+          { text: "Pivot: General Gaming piece, learnings in future articles", done: 1 },
+          { text: "Write First Draft", done: 1 },
+          { text: "Implement Feedback", done: 0 },
+          { text: "Polish Article, Add Images", done: 0 },
 
-
-/*
-{
-  title: "Research",
-  description: "Exploring new technologies and concepts",
-  colorClass: "rgb(105, 73, 242)",
-  type: "research", // Added type
-  todos: [
-    {
-      title: "Layer 2 Solutions",
-      percentage: 75,
-      items: ["Optimistic Rollups", "ZK Rollups", "Plasma", "State Channels", "Comparative Analysis"]
-    },
-    {
-      title: "Decentralized Identity",
-      percentage: 40,
-      items: ["W3C Standards", "DID Methods", "Verifiable Credentials", "Use Cases", "Implementation Challenges"]
-    }
-  ]
-},
-{
-  title: "Skill Development",
-  description: "Learning new programming languages",
-  colorClass: "rgb(204, 14, 191)",
-  type: "skill", // Added type
-  todos: [
-    {
-      title: "JavaScript",
-      percentage: 50,
-      items: ["Basics", "Advanced Concepts", "Frameworks"]
-    }
-  ]
-}
-  {
-    title: "Daily Threads",
-    description: "Trying to push a thread a day. From the 16th - 22nd.",
-    colorClass: "rgb(192, 255, 102)",
-    type: "skill",
-    todos: [
-      {
-        title: "On Doing the Bit",
-        percentage: 100,
-        items: [
-          { text: "Posted on X", done: 1, link: "https://x.com/ShuiTangs/status/1835606660364091844" },
-          { text: "Title Image", done: 1 },
-          { text: "Writing Done", done: 1 },
-          { text: "Initital Research", done: 1 },
         ]
       },
       {
-        title: "Fragmentation Quickie",
-        percentage: 0,
+        title: "Publish",
+        percentage: 15,
         items: [
-          { text: "Posted on X", done: 1, link: "https://x.com/ShuiTangs/status/1836013031987233031" }
-        ]
-      },
-      {
-        title: "What happened to Heroglyphs",
-        percentage: 0,
-        items: [
-          { text: "To be updated", done: 0 }
-        ]
-      },
-      {
-        title: "Uniswap Profitability",
-        percentage: 0,
-        items: [
-          { text: "To be updated", done: 0 }
-        ]
-      },
-      {
-        title: "Has Crypto gotten safer?",
-        percentage: 0,
-        items: [
-          { text: "To be updated", done: 0 }
-        ]
-      },
-      {
-        title: "Iterating on the Attention Economy",
-        percentage: 0,
-        items: [
-          { text: "To be updated", done: 0 }
-        ]
-      },
-      {
-        title: "Article: Aerodrome",
-        percentage: 0,
-        items: [
-          { text: "To be updated", done: 0 }
+          { text: "Deliver First Draft to Od1n, Oded, etc.", done: 1 },
+          { text: "Deliver Final Article", done: 0 },
+          { text: "Create X Article Overview", done: 0 },
+          { text: "Push Article to Site", done: 0 },
+          { text: "Create KOL list", done: 0 },
+          { text: "Publish", done: 0 }
         ]
       }
     ]
   },
-*/
+
+{
+  title: "Website",
+  description: "Adding functionality and improving UX",
+  colorClass: "rgb(105, 73, 242)",
+  type: "skill", // Added type
+  todos: [
+    {
+      title: "Features",
+      percentage: 100,
+      items: [
+        { text: "Newsletter Pup-up", done: 1 }
+      ]
+    },
+    {
+      title: "Backend",
+      percentage: 30,
+      items: [
+        { text: "Local date calculation", done: 1 },
+        { text: "Activity Workflow, mobile", done: 0 },
+        { text: "Project Workflow, mobile", done: 0 }
+      ]
+    },
+    {
+      title: "UX",
+      percentage: 40,
+      items: [
+        { text: "Writing Curation + Overhaul", done: 0 },
+        { text: "Calendar ToM", done: 0 },
+        { text: "General Layout", done: 0 }
+      ]
+    }
+  ]
+}
 ];
