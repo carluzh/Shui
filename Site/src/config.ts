0f93ef3c3c9e7831fb7040ab70b2d7ce3ecb032b
carluzh <164798883+carluzh@users.noreply.github.com>
2024-10-07 10:56:31 +0200

    Delete Site directory

--- Site/src/config.ts
+++
@@ -1,4 +0,0 @@
-export const SITE_TITLE = "Shui";
-export const SITE_DESCRIPTION =
-  "Independent Researcher. Crypto Enthusiast. Student.";
-export const SITE_AUTHOR = "Shui Tangs";
--- Site/src/data/home.ts
+++
@@ -1,2 +0,0 @@
-export const BUILDING_PROJECTS = [
-];
